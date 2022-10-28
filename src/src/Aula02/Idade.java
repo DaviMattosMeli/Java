2907ee5a994657220e938e03b791b56528193dd0
DaviMattosMeli <davi.mattos@mercadolivre.com>
2022-10-28 11:34:45 -0300

    Segundo Exercicio Java Aula 02

---
+++ src/src/Aula02/Idade.java
@@ -0,0 +1,31 @@
+package Aula02;
+
+import java.util.Scanner;
+
+public class Idade {
+
+    public static void main(String[] args) {
+
+        Scanner leia = new Scanner(System.in);
+
+        float idade;
+
+        System.out.println("Insira a sua idade");
+        idade = leia.nextFloat();
+
+
+        if(idade >= 10 && idade <= 14) {
+            System.out.println("10-14 Infantil");
+        }
+        else if(idade >=15 && idade <=17) {
+            System.out.println("10-14 Adolescente");
+        }else if(idade >=18 && idade <= 25){
+            System.out.println("18-15 Adulto");
+        }else {
+            System.out.println("tente novamente");
+        }
+
+
+    }
+
+}
