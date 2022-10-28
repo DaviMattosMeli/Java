c20ad83f84cd4f51c0466ba704cdd5b8bcac4c23
DaviMattosMeli <davi.mattos@mercadolivre.com>
2022-10-28 11:37:04 -0300

    Terceiro Exercicio Java Aula 02

--- src/src/Aula02/Idade.java
+++ src/src/Aula02/Idade.java
@@ -15,14 +15,17 @@ public class Idade {
 
 
         if(idade >= 10 && idade <= 14) {
-            System.out.println("10-14 Infantil");
+            System.out.println("10-14: Infantil");
         }
         else if(idade >=15 && idade <=17) {
-            System.out.println("10-14 Adolescente");
-        }else if(idade >=18 && idade <= 25){
-            System.out.println("18-15 Adulto");
+            System.out.println("15-17: Adolescente");
+        }else if(idade >=18 && idade <= 25) {
+            System.out.println("18-15: Adulto");
+        }
+        else if(idade >= 65) {
+                System.out.println("65+ : 7Idoso");
         }else {
-            System.out.println("tente novamente");
+            System.out.println("Tente novamente");
         }
 
 
